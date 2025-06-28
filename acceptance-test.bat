394714e3394bc618f74db02ef85caaf5e25887f5
nekoeyes26 <nekoeyes26@gmail.com>
2025-06-28 15:28:23 +0700

    edit acceptance

--- acceptance-test.bat
+++ acceptance-test.bat
@@ -1,16 +1,27 @@
 @echo off
-REM Menentukan profil staging
 set PROFILE=staging
 set CONTEXT=%PROFILE%
 
-REM Ambil IP cluster staging
-for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
-
-REM Ubah context kubectl agar mengarah ke staging
+echo Switching kubectl context to %CONTEXT%...
 kubectl config use-context %CONTEXT%
 
-REM Ambil port service dari staging
-for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
+echo Getting Minikube IP for profile: %PROFILE%...
+for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
+echo NODE_IP=%NODE_IP%
 
-REM Jalankan acceptance test
+echo Getting NodePort for calculator-service...
+for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
+echo NODE_PORT=%NODE_PORT%
+
+REM Validasi NODE_IP dan NODE_PORT
+if "%NODE_IP%"=="" (
+    echo ERROR: NODE_IP is empty!
+    exit /b 1
+)
+if "%NODE_PORT%"=="" (
+    echo ERROR: NODE_PORT is empty!
+    exit /b 1
+)
+
+echo Running acceptance test against http://%NODE_IP%:%NODE_PORT%
 gradlew.bat acceptanceTest "-Dcalculator.url=http://%NODE_IP%:%NODE_PORT%"
