958cd2c1c96d99a5941213093cc3101a9bd520f9
nekoeyes26 <nekoeyes26@gmail.com>
2025-06-28 20:16:32 +0700

    edit acceptance

--- acceptance-test.bat
+++ acceptance-test.bat
@@ -7,33 +7,32 @@ set CONTEXT=%PROFILE%
 echo Switching kubectl context to %CONTEXT%...
 kubectl config use-context %CONTEXT%
 
-REM --- Get Minikube IP and NodePort ---
-for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
-for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
+REM --- Gunakan minikube service --url agar host bisa akses
+set CALCULATOR_URL=
+for /f "delims=" %%u in ('minikube -p %PROFILE% service calculator-service --url 2^>nul') do set CALCULATOR_URL=%%u
 
-if "!NODE_IP!"=="" (
-    echo ERROR: NODE_IP is empty!
-    exit /b 1
-)
-if "!NODE_PORT!"=="" (
-    echo ERROR: NODE_PORT is empty!
-    exit /b 1
+REM --- Fallback: jika gagal, gunakan IP + NodePort (jika tunnel gagal)
+if "!CALCULATOR_URL!"=="" (
+    echo minikube service --url failed, fallback ke IP + NodePort...
+
+    for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
+    for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
+
+    set CALCULATOR_URL=http://!NODE_IP!:!NODE_PORT!
 )
 
-set CALCULATOR_URL=http://!NODE_IP!:!NODE_PORT!
 echo CALCULATOR_URL=!CALCULATOR_URL!
 
-REM --- Tes HTTP dulu (opsional) ---
+REM --- Tes koneksi
 curl -s -o nul -w "%%{http_code}" "!CALCULATOR_URL!" > tmp_response.txt
 set /p RESPONSE=<tmp_response.txt
 echo HTTP response: !RESPONSE!
 
 if NOT "!RESPONSE!"=="200" (
-    echo ERROR: Service not responding
+    echo ERROR: Service not responding at !CALCULATOR_URL!
     exit /b 1
 )
 
-REM --- Jalankan gradle ---
 echo Running acceptance test on !CALCULATOR_URL!
 gradlew.bat acceptanceTest "-Dcalculator.url=!CALCULATOR_URL!"
 
