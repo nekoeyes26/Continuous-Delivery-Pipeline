942a304903efec85dcd5c512d63d6ea3158352ad
nekoeyes26 <nekoeyes26@gmail.com>
2025-06-29 22:58:43 +0700

    edit smoke-test

--- smoke-test.bat
+++ smoke-test.bat
@@ -3,40 +3,13 @@ setlocal enabledelayedexpansion
 set PROFILE=production
 set CONTEXT=%PROFILE%
 
-:: Debug info
 echo [DEBUG] Starting smoke-test.bat
 
 echo Switching kubectl context to %CONTEXT%...
 kubectl config use-context %CONTEXT%
 
-echo Getting Minikube IP for profile: %PROFILE%...
-for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
-echo NODE_IP=!NODE_IP!
-
-echo Getting NodePort for calculator-service...
-for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
-echo NODE_PORT=!NODE_PORT!
-
-if "!NODE_IP!"=="" (
-    echo ERROR: NODE_IP is empty!
-    exit /b 1
-)
-if "!NODE_PORT!"=="" (
-    echo ERROR: NODE_PORT is empty!
-    exit /b 1
-)
-
-set CALCULATOR_URL=http://!NODE_IP!:!NODE_PORT!
-echo CALCULATOR_URL=!CALCULATOR_URL!
-
-:: Extra check for special characters or empty value
-if "!CALCULATOR_URL!"=="" (
-    echo ERROR: CALCULATOR_URL is empty!
-    exit /b 1
-)
-
-:: Port forwarding ke 4445
-start /min cmd /c "kubectl port-forward svc/calculator-service 4445:8080 > portforward.log 2>&1"
+:: Port forwarding ke 4445 (background)
+start /min "" cmd /c "kubectl port-forward svc/calculator-service 4445:8080 > portforward.log 2>&1"
 
 :: Tunggu port forwarding siap (maks 20 detik)
 set /a COUNT=0
