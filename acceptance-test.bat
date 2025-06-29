dc67be4faf5866c525ea7cffef54759f459c6f49
nekoeyes26 <nekoeyes26@gmail.com>
2025-06-29 19:59:31 +0700

    edit acceptance

--- acceptance-test.bat
+++ acceptance-test.bat
@@ -1,24 +1,31 @@
 @echo off
+setlocal enabledelayedexpansion
 set PROFILE=staging
 set CONTEXT=%PROFILE%
 
+:: Debug info
+echo [DEBUG] Starting acceptance-test.bat
+
+:: Switch kubectl context
 echo Switching kubectl context to %CONTEXT%...
 kubectl config use-context %CONTEXT%
 
+:: Get Minikube IP
 echo Getting Minikube IP for profile: %PROFILE%...
 for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
-echo NODE_IP=%NODE_IP%
+echo NODE_IP=!NODE_IP!
 
+:: Get NodePort
 echo Getting NodePort for calculator-service...
 for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
-echo NODE_PORT=%NODE_PORT%
+echo NODE_PORT=!NODE_PORT!
 
-REM Validasi NODE_IP dan NODE_PORT
-if "%NODE_IP%"=="" (
+:: Validate NODE_IP and NODE_PORT
+if "!NODE_IP!"=="" (
     echo ERROR: NODE_IP is empty!
     exit /b 1
 )
-if "%NODE_PORT%"=="" (
+if "!NODE_PORT!"=="" (
     echo ERROR: NODE_PORT is empty!
     exit /b 1
 )
@@ -26,12 +33,23 @@ if "%NODE_PORT%"=="" (
 set CALCULATOR_URL=
 for /f "delims=" %%u in ('minikube -p %PROFILE% service calculator-service --url 2^>nul') do set CALCULATOR_URL=%%u
 
-if "%CALCULATOR_URL%"=="" (
+if "!CALCULATOR_URL!"=="" (
     echo minikube --url failed, falling back to IP + NodePort
     for /f "delims=" %%i in ('minikube -p %PROFILE% ip') do set NODE_IP=%%i
     for /f "delims=" %%j in ('kubectl get svc calculator-service -o "jsonpath={.spec.ports[0].nodePort}"') do set NODE_PORT=%%j
-    set CALCULATOR_URL=http://%NODE_IP%:%NODE_PORT%
+    set CALCULATOR_URL=http://!NODE_IP!:!NODE_PORT!
 )
 
-echo Running acceptance test against %CALCULATOR_URL%
-gradlew.bat acceptanceTest "-Dcalculator.url=%CALCULATOR_URL%"
+:: Debug CALCULATOR_URL
+echo CALCULATOR_URL=!CALCULATOR_URL!
+
+:: Extra check for special characters or empty value
+if "!CALCULATOR_URL!"=="" (
+    echo ERROR: CALCULATOR_URL is empty!
+    exit /b 1
+)
+
+:: Run acceptance test
+echo Running acceptance test against !CALCULATOR_URL!
+call gradlew.bat acceptanceTest -Dcalculator.url=!CALCULATOR_URL!
+endlocal
